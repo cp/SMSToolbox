89064feddd4d9a3987435f7b569eff1141f8ba2c
ColbyAley <colby@aley.me>
2012-12-29 19:45:24 -0800

    hooks up ebay

--- twilio.php
+++ twilio.php
@@ -1,34 +1,18 @@
 <?php
 
 // include all features classes
-//function __autoload($class_name) {
-//    include 'features/'.$class_name . '.php';
-//}
+function __autoload($class_name) {
+    include 'features/'.$class_name . '.php';
+}
 
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
-$urltoshorten = $_REQUEST['Body'];
+$keyword = $_REQUEST['Body'];
 
-
- function tinyurl() {
-	//takes text and returns tinyurl
-	$url = "http://tinyurl.com/api-create.php?url={$urltoshorten}";
-	$ch = curl_init();
-	
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
-	
-	$output = curl_exec($ch);
-	
-	curl_close($ch);
-}
-
-//eBay::getAvgPrice("keywords");
+$aprice = eBay::getAvgPrice($keyword);
 
 ?>
 <Response>
-    <Sms>Short URL: <?php echo $output ?></Sms>
+    <Sms>Average Price: <?php echo $aprice ?></Sms>
 </Response>
