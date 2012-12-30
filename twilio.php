2e99d7b0bbe57bd0a39c3b75265d163a21b2e3df
Quintin Cummins <quintincummins@hotmail.com>
2012-12-29 20:12:33 -0800

    Re made the main twilio class and encapsulated the data

--- twilio.php
+++ twilio.php
@@ -1,18 +1,64 @@
 <?php
+/***********************************************************************/
+/* Helper functions                                                    */
+/***********************************************************************/
 
-// include all features classes
+/**
+ * Automatically loads all classes within the features folder
+ * @param type $class_name
+ */
 function __autoload($class_name) {
-    include 'features/'.$class_name . '.php';
+    include 'features/' . $class_name . '.php';
 }
 
-header("content-type: text/xml");
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+/**
+ * Returns the app, feature and command past in from a sms message
+ * @param type $message
+ * @param type $app
+ * @param type $feature
+ * @param type $command
+ */
+function getCommand($message, &$app, &$feature, &$command) {
+    $args = split(" ", $message);
 
-$keyword = $_REQUEST['Body'];
+    $app = $args[0];
+    $feature = $args[1];
+    $command = "";
+    for ($i = 2; $i < sizeof($args); ++$i)
+        $command .= $args[$i];
+}
 
-$aprice = eBay::getAvgPrice($keyword);
+/***********************************************************************/
+/* Main entry function                                                 */
+/***********************************************************************/
+
+/**
+ * The main function that does everything
+ */
+function process() {
+
+    $app = "";
+    $feature = "";
+    $command = "";
+    getCommand($_REQUEST['Body'], $app, $feature, $command);
+
+    header("content-type: text/xml");
+    $msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+    $response = "";
+    switch ($app) {
+        case $app:
+            $response = ebay::process($feature, $command);
+            break;
+        default:
+            break;
+    }
+
+    $msg .= "<Response>
+                <Sms>$response</Sms>
+             </Response>";
+}
+
+process();
 
 ?>
-<Response>
-    <Sms>Average Price: <?php echo $aprice ?></Sms>
-</Response>
