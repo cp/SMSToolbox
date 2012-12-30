986e8cfa320e409c9ad391295754a12e9f5b72aa
Quintin Cummins <quintincummins@hotmail.com>
2012-12-29 19:27:10 -0800

    Put the ebay features into its own class

---
+++ features/ebay.php
@@ -0,0 +1,64 @@
+<?php
+
+class eBay {
+
+    /**
+     * Returns the average price for a list of keywords
+     * @param type $keywords
+     * @return type
+     */
+    public static function getAvgPrice($keywords) {
+        $endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';  // URL to call
+        $appid = 'QuintinC-6db3-4c4f-a824-e6dfbc49a102';  // You will need to supply your own AppID
+        $version = '1.0.0';  // API version supported by your application
+
+        $globalid = 'EBAY-US';
+
+        $safequery = urlencode($keywords);  // Make the query URL-friendly
+        // Construct getMostWatchedItems call with maxResults and categoryId as input
+        $apicalla = "$endpoint?";
+        $apicalla .= "OPERATION-NAME=findItemsByKeywords";
+        $apicalla .= "&SERVICE-VERSION=$version";
+        $apicalla .= "&SECURITY-APPNAME=$appid";
+        $apicalla .= "&GLOBAL-ID=$globalid";
+        $apicalla .= "&keywords=$safequery";
+        $apicalla .= "&paginationInput.entriesPerPage=20";
+
+        // Load the call and capture the document returned by eBay API
+        $resp = simplexml_load_file($apicalla);
+
+        // Check to see if the response was loaded, else print an error
+        if ($resp) {
+            // Set return value for the function to null
+            $retna = '';
+
+            // Verify whether call was successful
+            if ($resp->ack == "Success") {
+
+                // If there were no errors, build the return response for the function
+
+                $results = "";
+                $avg = 0;
+                $itemCount = 0;
+                foreach ($resp->searchResult->item as $item) {
+                    // print_r($item);
+                    // $link = $item->viewItemURL;
+                    // $title = $item->title;
+                    $price = $item->sellingStatus->currentPrice;
+
+                    // echo $title . "<br />" . $price . "<br />";
+                    $avg += $price;
+                    $itemCount++;
+                }
+                $avg = $avg / $itemCount;
+                echo "Avg: " . $avg;
+            }
+        } else {
+            $retna .= "Call used was: $apicalla";
+        }  // End if response exists
+        // Return the function's value
+        return $retna;
+    }
+
+}
+?>
