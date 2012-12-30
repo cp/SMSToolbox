39ac11e1660930da8b519f62faf668a8a2903388
ColbyAley <colby@aley.me>
2012-12-29 19:22:01 -0800

    Add twilio base

---
+++ twilio.php
@@ -0,0 +1,24 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+   
+$urltoshorten = $_REQUEST['Body'];
+
+//get url back, get short url
+$url = "http://tinyurl.com/api-create.php?url={$urltoshorten}";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+$output = curl_exec($ch);
+
+curl_close($ch);
+
+?>
+<Response>
+    <Sms>Short URL: <?php echo $output ?></Sms>
+</Response>
+
