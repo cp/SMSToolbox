2a4eedc42854648a0b71c5333a11c345fc59462c
Chance Zibolsk <chance.zibolski@gmail.com>
2012-12-29 20:26:31 -0800

    Store all message attributes into single array

--- utils.php
+++ utils.php
@@ -4,16 +4,16 @@ function shortenurl($urltoshorten){
 	//takes text and returns tinyurl
 	$tiny = "http://tinyurl.com/api-create.php?url={$urltoshorten}";
 	$ch = curl_init();
-	
+
 	curl_setopt($ch, CURLOPT_URL, $tiny);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	
-	
+
+
 	$output = curl_exec($ch);
-	
+
 	curl_close($ch);
-	
+
 	return $output
 
 }
