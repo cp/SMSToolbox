175fe81a0fd9cc4670c3d792da1abb37493af773
ColbyAley <colby@aley.me>
2012-12-29 19:40:28 -0800

    twilio

--- twilio.php
+++ twilio.php
@@ -4,18 +4,21 @@
    
 $urltoshorten = $_REQUEST['Body'];
 
-//get url back, get short url
-$url = "http://tinyurl.com/api-create.php?url={$urltoshorten}";
-$ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-
-$output = curl_exec($ch);
-
-curl_close($ch);
+ function tinyurl() {
+	//takes text and returns tinyurl
+	$url = "http://tinyurl.com/api-create.php?url={$urltoshorten}";
+	$ch = curl_init();
+	
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+	
+	$output = curl_exec($ch);
+	
+	curl_close($ch);
+}
 
 ?>
 <Response>
