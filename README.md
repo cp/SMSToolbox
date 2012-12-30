7da47ceb09c66d556aac75366a25f5e6e27af568
ColbyAley <colby@aley.me>
2012-12-30 00:04:53 -0800

    fixed weather ish and updates readme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Balls
 Super awesome service that allows you to rule the world over SMS.
 
 Balls is devided into apps, features, and commands. For example Ebay would be an app, avgprice would be a feature, and skateboard would be a command.
+More symply put, the syntax is "<app> <feature> <command>" seperated by spaces.
 
 #Services
 
@@ -12,11 +13,15 @@ Balls is devided into apps, features, and commands. For example Ebay would be an
 This takes a keyword, searches Ebay, and finds the average price of a product.
 
 ##Weather
-###Current
-This gets the current temperature and condition from your location. (in development)
-###24hours
-This gets the forcast for the next 24 hours.
+###condition
+This returns the condition, such as 'clear' or 'partly cloudy'.
+###temperature
+This returns the temperature in fahrenheit.
+
 ##Bus Times
+
 ##YouTube
+
 ##Ski Conditions
+
 ##Fandango / Movie Times
--- features/weather.php
+++ features/weather.php
@@ -4,12 +4,15 @@ class weather {
 
 
 	public static function process($body) {
-        switch($body) {
+        switch($body['feature']) {
             case "condition":
                 return weather::condition($body['command']);
 				break;
+			case "temperature":
+                return weather::temperature($body['command']);
+				break;
             default:
-                echo "Feature not found!";
+                return "Feature not found!";
                 break;
         }
     }
@@ -22,15 +25,35 @@ class weather {
 				  $FromState = $_REQUEST['FromState'];
 				  $FromZip = $_REQUEST['FromZip'];
 				  
-				  $json_string = file_get_contents("http://api.wunderground.com/api/3b6f07179e6e0f97/geolookup/conditions/q/{$FromState}/{$FromCity}.json");
+				  $json_string = file_get_contents("http://api.wunderground.com/api/3ec99dbc9c7f8d6a/geolookup/conditions/q/{$FromState}/{$FromCity}.json");
 				  $parsed_json = json_decode($json_string);
 				  $location = $parsed_json->{'location'}->{'city'};
-				  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
-				  $text =  "Current temperature in {$FromCity} is: {$temp_f}";
+				  $weather = $parsed_json->{'current_observation'}->{'weather'};
+				  $text =  "Current condition in {$FromCity} is: {$weather}";
 				  return $text;
 				break;
 			default:
-				echo "Please select a time";
+				return "Please select a time";
+				break;
+		}
+    }
+    //Gets weather condition
+    public static function temperature($command) {
+		switch($command) {
+			case 'current':
+				  $FromCity = $_REQUEST['FromCity'];
+				  $FromState = $_REQUEST['FromState'];
+				  $FromZip = $_REQUEST['FromZip'];
+				  
+				  $json_string = file_get_contents("http://api.wunderground.com/api/3ec99dbc9c7f8d6a/geolookup/conditions/q/{$FromState}/{$FromCity}.json");
+				  $parsed_json = json_decode($json_string);
+				  $location = $parsed_json->{'location'}->{'city'};
+				  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
+				  $text =  "Current condition in {$FromCity} is: {$temp_f}";
+				  return $text;
+				break;
+			default:
+				return "Please select a time";
 				break;
 		}
     }
