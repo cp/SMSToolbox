4f89b961f6efa67c45f4a97bbae954e0cb4271a7
ColbyAley <colby@aley.me>
2012-12-29 22:08:22 -0800

    Removes Amazon, starts weather

--- features/weather.php
+++ features/weather.php
@@ -1,18 +1,27 @@
 <?php
 
-class eBay {
+class Weather {
 
     public static function process($feature, $command) {
         switch($feature) {
-            case "avg":
-                return eBay::getAvgPrice($command);
+            case "weather":
+                return Weather::getAvgPrice($command);
             default:
                 echo "Feature not found!";
                 break;
         }
     }
-    public static function getAverage($keywords) {
-	    //GET THE DATA
+    public static function getCurrentTemp($keywords) {
+    
+	    $FromCity=$_REQUEST['FromCity'];
+        $FromState=$_REQUEST['FromState'];
+        $FromZip=$_REQUEST['FromZip'];
+    
+	  $json_string = file_get_contents("http://api.wunderground.com/api/3ec99dbc9c7f8d6a/geolookup/conditions/q/IA/Cedar_Rapids.json");
+	  $parsed_json = json_decode($json_string);
+	  $location = $parsed_json->{'location'}->{'city'};
+	  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
+	  return "Current temperature in Portland is: {$FromCity}";
     }
     
 ?>
